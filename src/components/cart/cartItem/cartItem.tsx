7f32b15e72a452f6112d8a412ca6c23d9926025c
Lautaro <lausuarez71@gmail.com>
2023-03-24 19:30:37 +0100

    fix new changes regarding tests

--- src/components/cart/cartItem/cartItem.tsx
+++ src/components/cart/cartItem/cartItem.tsx
@@ -1,7 +1,8 @@
 import { Fragment, useState } from 'react'
-import { incrementQuantity, decrementQuantity,removeItem} from 'redux/reducers/cartReducer'
-import { useDispatch } from 'react-redux'
-import CartBottom from '../cartBotton/cartBottom'
+import { Dialog, Transition } from '@headlessui/react'
+import { incrementQuantity, decrementQuantity, addToCart,removeItem} from 'redux/reducers/cartReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import CartBottom from 'components/cart/cartBotton/cartBottom'
 import ArrowLeft from 'components/arrowLeft/arrowLeft'
 
 interface Cart {
