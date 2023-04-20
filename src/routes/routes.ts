52051f602f5581ff7c7141b26c8e13f3e3dcd179
lausuarez02 <lausuarez71@gmail.com>
2023-04-20 14:05:39 +0200

    fix: Low fix change route for back

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,7 @@
 const routes = {
     "products":{
         "mainRoute": "https://amazon24.p.rapidapi.com/api/category",
-        "testRoute": "http://127.0.0.1:5050/data"
+        "testRoute": "http://127.0.0.1:5050/v1/all-products"
     }
 }
 
