cc205369bbcb10ddabd6d1bab643dc841c9655ee
lausuarez02 <lausuarez71@gmail.com>
2023-04-26 16:25:51 +0200

    Fix when url changes params

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -3,16 +3,24 @@ import Item from 'components/item/item'
 import {Header} from 'components/header/header'
 //helpers 
 import routes from 'routes/routes'
-import { useSelector } from 'react-redux';
-import {useLocation} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {useLocation, useSearchParams} from 'react-router-dom';
 import NotFound from 'components/notFound/notFound'
 //custom hooks
 import useFetch from 'hooks/fetchHook/fetchHook';
+import { useEffect } from 'react';
+import { mainSearchData } from 'redux/reducers/searchReducer';
 
 function Home (){
+  const dispatch = useDispatch()
   const { products: { testRoute}} = routes
   const pathName = useLocation().pathname
+  //Getting the params from ?search=something
+  const [searchParams] = useSearchParams();
+  const urlValue = searchParams.get('search')
+  console.log(searchParams.get('search'), "Testing search Params")
 
+  //fetching the data
   const { error, data } = useFetch({
     url: new URL(testRoute),
   })
@@ -20,9 +28,12 @@ function Home (){
  
   //getting the data from the search the user made
   const search = useSelector((state:any) => (console.log(state, "test"), state.search.dataSearch))
-  
   // Getting the last search from the array made when the user make a search
   const lastSearch = search[search.length -1]
+  //dispatch data if user changes the value directly from the url
+  if(lastSearch !== urlValue ){
+    dispatch(mainSearchData(urlValue))
+  }
   
   if(pathName !== '/products'){
     return <NotFound/>
