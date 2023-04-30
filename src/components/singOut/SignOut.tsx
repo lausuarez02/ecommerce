06874aee1d71a773f8148f81b2c0fbd105428313
lausuarez02 <lausuarez71@gmail.com>
2023-04-30 20:27:59 +0200

    Fix login page

--- src/components/singOut/SignOut.tsx
+++ src/components/singOut/SignOut.tsx
@@ -7,7 +7,7 @@ const SingOut = () => {
     AuthState()
     return(
         <div>
-            <button onClick={() => UserSingOutGoogle()}>Sing Out</button>
+            <button className="flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={() => UserSingOutGoogle()}>Sing Out</button>
         </div>
     )
 }
