86821c384cb43f61a14145eb14ea8b23251fb7d2
lausuarez02 <lausuarez71@gmail.com>
2023-05-01 16:10:24 +0200

    Fix product view

--- src/views/product/Product.tsx
+++ src/views/product/Product.tsx
@@ -1,10 +1,30 @@
 import React, {useState, useEffect} from 'react'
 //components
 import Products from 'components/product/product'
+import CartIcon from 'components/cart/cartIcon/cartIcon';
+import ArrowLeft from 'components/arrowLeft/arrowLeft'
 
 const Product = () => {
     return(
         <div data-testid="product">
+                 <nav aria-label="Top" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              {/* Logo */}
+              <div className="ml-4 flex lg:ml-0">
+              <ArrowLeft/>
+              </div>
+              {/* Flyout menus */}
+              <div className="ml-auto flex items-center">
+                <div className="ml-4 flow-root lg:ml-6">
+                  <div >
+                  <CartIcon/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>      
             <Products data-testid="products"/>
         </div>
     )
